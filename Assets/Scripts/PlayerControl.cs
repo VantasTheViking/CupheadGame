a3ab3756494a2e6820cb35968ea36ab69c2299b8
Blobmaster2 <scott.murphy2@ontariotechu.net>
2021-11-11 00:18:19 -0500

    1.1
    Added shooting and smoother diagonal movements

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,128 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    Transform trans;
+    Rigidbody2D body;
+
+    [SerializeField] float moveSpeed;
+
+    [SerializeField] GameObject bulletPrefab;
+    [SerializeField] Transform bulletSpawn;
+    [SerializeField] float bulletSpeed;
+    [SerializeField] float shootDelay;
+
+    float timeToNextShot = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        trans = GetComponent<Transform>();
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    private void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.Space) && CanShoot())
+        {
+            ShootBullet();
+        }
+    }
+
+    void ShootBullet()
+    {
+        var bullet = Instantiate(bulletPrefab, bulletSpawn.position, Quaternion.Euler(new Vector3(0, 0, 90)));
+
+        bullet.GetComponent<Rigidbody2D>().velocity = transform.up * bulletSpeed;
+
+        Destroy(bullet, 5);
+    }
+
+    bool CanShoot()
+    {
+        if (timeToNextShot < Time.realtimeSinceStartup)
+        {
+            timeToNextShot = Time.realtimeSinceStartup + shootDelay;
+            return true;
+        }
+
+        else
+        {
+            return false;
+        }
+    }
+
+    void Move()
+    {
+        List<string> inputs = new List<string>();
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            inputs.Add("w");
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            inputs.Add("s");
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            inputs.Add("d");
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            inputs.Add("a");
+        }
+
+        AssignInput(inputs);
+
+        inputs.Clear();
+    }
+
+    //specifically for diagonal movement; makes the movement more smooth
+    void AssignInput(List<string> inputs)
+    {
+        if (inputs.Count == 2)
+        {
+            MovePlayer(inputs, 1.5f);
+        }
+
+        else
+        {
+            MovePlayer(inputs, 1);
+        }
+    }
+
+    void MovePlayer(List<string> inputs, float divider)
+    {
+        for (int x = 0; x < inputs.Count; x++)
+        {
+            if (inputs[x] == "w")
+            {
+                trans.position += -transform.right * Time.deltaTime * moveSpeed / divider;
+            }
+            else if (inputs[x] == "s")
+            {
+                trans.position += transform.right * Time.deltaTime * moveSpeed / divider;
+            }
+            else if (inputs[x] == "d")
+            {
+                trans.position += transform.up * Time.deltaTime * moveSpeed / divider;
+            }
+            else if (inputs[x] == "a")
+            {
+                trans.position += -transform.up * Time.deltaTime * moveSpeed / divider;
+            }
+        }
+    }
+}
