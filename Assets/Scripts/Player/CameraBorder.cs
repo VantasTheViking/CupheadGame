05ae6808ebf8113141d096fceebc17a27bd8fae4
Blobmaster2 <scott.murphy2@ontariotechu.net>
2021-11-24 16:18:19 -0500

    1.8
    Background will now fit to camera size no matter the initial scale / position

--- Assets/Scripts/Player/CameraBorder.cs
+++ Assets/Scripts/Player/CameraBorder.cs
@@ -6,6 +6,7 @@ public class CameraBorder : MonoBehaviour
 {
     [SerializeField] Transform _playerPos;
     [SerializeField] Camera _camera;
+    [SerializeField] GameObject background;
 
     Vector3 bL;
     Vector3 tR;
@@ -14,19 +15,23 @@ public class CameraBorder : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        
 
         bL = _camera.ScreenToWorldPoint(Vector3.zero);
 
         tR = _camera.ScreenToWorldPoint(new Vector3(_camera.pixelWidth, _camera.pixelHeight));
 
         camRect = new Rect(bL.x, bL.y, tR.x - bL.x, tR.y - bL.y);
+
+        background.transform.localScale = new Vector3(camRect.size.x, camRect.size.y, 1);
+        background.transform.position = new Vector3(0, 0, 0);
     }
 
 
     // Update is called once per frame
     void Update()
     {
-        _playerPos.transform.position = new Vector3(Mathf.Clamp(transform.position.x, camRect.xMin + 0.5f, camRect.xMax - 0.5f), Mathf.Clamp(transform.position.y, camRect.yMin + 0.25f, camRect.yMax - 0.25f), transform.position.z);
+        _playerPos.transform.position = new Vector3(Mathf.Clamp(_playerPos.transform.position.x, camRect.xMin + 0.5f, camRect.xMax - 0.5f), Mathf.Clamp(_playerPos.transform.position.y, camRect.yMin + 0.25f, camRect.yMax - 0.25f), _playerPos.transform.position.z);
     }
 }
 
